aeca4c307ce5960e29caed626514307383a073e5
Abhijithnothing <a00931668@gmail.com>
2025-10-04 11:10:05 +0530

    withdraw

--- bank.py
+++ bank.py
@@ -1,6 +1,14 @@
-initial_bal=float(input("Enter the initial balance in your bank account: "))
-deposit=float(input("Enter the amount to be deposited: "))
 
-print(f"Initial Balance: {initial_bal:.2f}")
-new_bal=initial_bal+deposit
+initial_bal = float(input("Enter the initial balance in your bank account: "))
+deposit = float(input("Enter the amount to be deposited: "))
+print(f"\nInitial Balance: {initial_bal:.2f}")
+new_bal = initial_bal + deposit
 print(f"New Balance after deposit of {deposit:.2f}: {new_bal:.2f}")
+withdraw = float(input("\nEnter the amount to withdraw: "))
+if withdraw > new_bal:
+    print("Insufficient balance! Withdrawal not possible.")
+else:
+    new_bal -= withdraw
+    print(f"New Balance after withdrawing {withdraw:.2f}: {new_bal:.2f}")
+
+print("\nFinal Balance:", f"{new_bal:.2f}")
